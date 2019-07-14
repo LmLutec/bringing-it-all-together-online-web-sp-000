7ff144131de07ccbecdc19bf28748b2c1c1553eb
Lauren McCoy <lmccoy8911@gmail.com>
2019-07-14 15:15:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -26,8 +26,9 @@ class Dog
     else 
     sql = "INSERT INTO dogs (name, breed) VALUES (?,?)"
     DB[:conn].execute(sql, self.name, self.breed)
-    self 
+    @id= DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end 
+    self 
   end 
   
   def self.create(name:, breed:) 
@@ -57,6 +58,12 @@ class Dog
   
   def self.new_from_db(row)
     dog = self.new(id: row[0], name: row[1], breed: row[2]) 
+  end 
+  
+  def self.find_by_name(name)
+    sql = "SELECT name FROM dogs WHERE name = #{name}"
+    dog = DB[:conn].execute(sql)
+    binding.pry 
   end 
   
   def update
